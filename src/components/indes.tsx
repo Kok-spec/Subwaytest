88f32bfe9aadf2dfccb24d2820a965f69726cb9d
Kok-spec <kokoa.kawaraya.yw@ps.hitachi-solutions.com>
2025-12-11 14:41:54 +0900

    002

--- src/components/indes.tsx
+++ src/components/indes.tsx
@@ -1,5 +1,5 @@
 
-import haikei from "./stryt.png";
+import stryt from "./stryt.png";
 import PanoramaViewer from "./PanoramaViewer";
 import { useState } from "react";
 
@@ -13,57 +13,43 @@ function StationMap({ className = "" }: Props) {
   return (
     <div
       className={`absolute inset-0 m-auto w-[1147px] h-[783px] bg-cover bg-[50%_50%] ${className}`}
-      style={{ backgroundImage: `url(${haikei})` }}
+      style={{ backgroundImage: `url(${stryt})` }}
     >
       {/* 各ボタン */}
       <button
-        className={`absolute w-[3.40%] h-[4.85%] top-[64.11%] left-[89.45%] bg-[#fdf8f8b0] rounded-[19.5px/19px] ${
+        className={`absolute w-[2.12%] h-[4.62%] top-[55.71%] left-[73.25%] bg-[#d9d9d9] rounded-[8.5px] shadow-[0px_4px_4px_#00000040] ${
           viewIndex === 0 ? "selected-button" : ""
         }`}
         onClick={() => setViewIndex(0)}
         aria-label="view 0"
       />
       <button
-        className={`absolute w-[3.40%] h-[4.85%] top-[49.17%] left-[68.44%] bg-[#fdf8f8b0] rounded-[19.5px/19px] ${
+        className={`absolute w-[2.12%] h-[4.62%] top-[58.97%] left-[63.00%] bg-[#d9d9d9] rounded-[8.5px] shadow-[0px_4px_4px_#00000040] ${
           viewIndex === 1 ? "selected-button" : ""
         }`}
         onClick={() => setViewIndex(1)}
         aria-label="view 1"
       />
       <button
-        className={`absolute w-[3.40%] h-[4.85%] top-[42.15%] left-[73.23%] bg-[#fdf8f8b0] rounded-[19.5px/19px] ${
+        className={`absolute w-[2.12%] h-[4.62%] top-[58.97%] left-[67.50%] bg-[#d9d9d9] rounded-[8.5px] shadow-[0px_4px_4px_#00000040] ${
           viewIndex === 2 ? "selected-button" : ""
         }`}
         onClick={() => setViewIndex(2)}
         aria-label="view 2"
       />
       <button
-        className={`absolute w-[3.40%] h-[4.85%] top-[52.62%] left-[76.29%] bg-[#fdf8f8b0] rounded-[19.5px/19px] ${
+        className={`absolute w-[2.12%] h-[4.62%] top-[63.59%] left-[69.62%] bg-[#d9d9d9] rounded-[8.5px] shadow-[0px_4px_4px_#00000040] ${
           viewIndex === 3 ? "selected-button" : ""
         }`}
         onClick={() => setViewIndex(3)}
         aria-label="view 3"
       />
       <button
-        className={`absolute w-[3.40%] h-[4.85%] top-[65.39%] left-[78.47%] bg-[#fdf8f8b0] rounded-[19.5px/19px] ${
+        className={`absolute w-[2.12%] h-[4.62%] top-[55.71%] left-[69.88%] bg-[#d9d9d9] rounded-[8.5px] shadow-[0px_4px_4px_#00000040] ${
           viewIndex === 4 ? "selected-button" : ""
         }`}
         onClick={() => setViewIndex(4)}
         aria-label="view 4"
-      />
-      <button
-        className={`absolute w-[3.40%] h-[4.85%] top-[76.63%] left-[78.81%] bg-[#fdf8f8b0] rounded-[19.5px/19px] ${
-          viewIndex === 5 ? "selected-button" : ""
-        }`}
-        onClick={() => setViewIndex(5)}
-        aria-label="view 5"
-      />
-      <button
-        className={`absolute w-[3.40%] h-[4.85%] top-[74.71%] left-[69.31%] bg-[#fdf8f8b0] rounded-[19.5px/19px] ${
-          viewIndex === 6 ? "selected-button" : ""
-        }`}
-        onClick={() => setViewIndex(6)}
-        aria-label="view 6"
       />
 
       {/* 画像出力 */}
