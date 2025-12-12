af56b01dbe9f143b1e109f0fa885132d287df2b8
Kok-spec <kokoa.kawaraya.yw@ps.hitachi-solutions.com>
2025-12-12 12:30:22 +0900

    014

--- src/components/StationMap.tsx
+++ src/components/StationMap.tsx
@@ -17,35 +17,35 @@ function StationMap({ className = "" }: Props) {
     >
       {/* 各ボタン */}
       <button
-        className={`absolute w-[2.12%] h-[4.62%] top-[54.80%] left-[73.25%] bg-[#d9d9d9] rounded-[8.5px] shadow-[0px_4px_4px_#00000040] ${
+        className={`absolute w-[2.12%] h-[4.32%] top-[54.80%] left-[73.25%] rounded-[8.5px] rounded-full ${
           viewIndex === 0 ? "selected-button" : ""
         }`}
         onClick={() => setViewIndex(0)}
         aria-label="view 0"
       />
       <button
-        className={`absolute w-[2.12%] h-[4.62%] top-[57%] left-[63.00%] bg-[#d9d9d9] rounded-[8.5px] shadow-[0px_4px_4px_#00000040] ${
+        className={`absolute w-[2.12%] h-[4.32%] top-[57%] left-[63.00%] rounded-[8.5px] rounded-full ${
           viewIndex === 3 ? "selected-button" : ""
         }`}
         onClick={() => setViewIndex(3)}
         aria-label="view 3"
       />
       <button
-        className={`absolute w-[2.12%] h-[4.62%] top-[57.80%] left-[67.50%] bg-[#d9d9d9] rounded-[8.5px] shadow-[0px_4px_4px_#00000040] ${
+        className={`absolute w-[2.12%] h-[4.32%] top-[57.80%] left-[67.50%] rounded-[8.5px] rounded-full ${
           viewIndex === 2 ? "selected-button" : ""
         }`}
         onClick={() => setViewIndex(2)}
         aria-label="view 2"
       />
       <button
-        className={`absolute w-[2.12%] h-[4.62%] top-[62%] left-[69.62%] bg-[#d9d9d9] rounded-[8.5px] shadow-[0px_4px_4px_#00000040] ${
+        className={`absolute w-[2.12%] h-[4.32%] top-[62%] left-[69.62%] rounded-[8.5px] rounded-full ${
           viewIndex === 4 ? "selected-button" : ""
         }`}
         onClick={() => setViewIndex(4)}
         aria-label="view 4"
       />
       <button
-        className={`absolute w-[2.12%] h-[4.62%] top-[55.50%] left-[69.88%] bg-[#d9d9d9] rounded-[8.5px] shadow-[0px_4px_4px_#00000040] ${
+        className={`absolute w-[2.12%] h-[4.32%] top-[55.50%] left-[69.88%] rounded-[8.5px] rounded-full ${
           viewIndex === 1 ? "selected-button" : ""
         }`}
         onClick={() => setViewIndex(1)}
